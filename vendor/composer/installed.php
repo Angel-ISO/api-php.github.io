8f2a1952ec9959d567fbd01e71aa57fa31f52cf8
Angel-ISO <angelgabrielorteg@gmail.com>
2023-06-06 09:17:35 -0500

    feat(plantilla base con composer): :art: nueva insersion a mysql con formulario
    insersion a mysql con composer almasenado en mysql
    
    ll

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'desarrollo/bdphp-conex',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a7cc70f28cfaaba8e7f49f5580d91607cf9d9655',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'desarrollo/bdphp-conex' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a7cc70f28cfaaba8e7f49f5580d91607cf9d9655',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
